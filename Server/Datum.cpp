6e2574468d57fbe84166ae371bec3026174a6500
19097395 <56761664+19097395@users.noreply.github.com>
2022-01-20 17:58:23 +0100

    Update Datum.cpp

--- Server/Datum.cpp
+++ Server/Datum.cpp
@@ -7,11 +7,11 @@ Datum::Datum(){}
 //Return current date and time
 std::ostream& operator<<(std::ostream& s, const Datum& ds)
 {
-    time_t now = time(0);
-    char* dt = ctime(&now);
+    time_t now = time(0); //haal huidige tijd op
+    char* dt = ctime(&now); //zet tijd om in standaart leesbaar formaat in char*
     if (dt[strlen(dt)-1] == '\n')
-    dt[strlen(dt)-1] = '\0';
+    dt[strlen(dt)-1] = '\0'; //haal enter weg op de laatste regel: staat niet netjes in logboek
 
-    s<<dt<<":::";
-    return s;
+    s<<dt<<":::"; //voeg datumstring toe aan de huidige string met dubbele punten voor leesbaarheid
+    return s; //return voor overloading
 }
